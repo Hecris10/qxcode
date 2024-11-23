94f45aee6938b41fff6c288e96515958cc339b62
Helamã Ewerton <helamaewerton@gmail.com>
2024-11-23 03:17:34 -0700

    feat: Enhance middleware for authentication handling and redirect logic based on user authentication status

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,35 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export function middleware(request: Request) {
-  // Store current request url in a custom header, which you can read later
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-url", request.url);
+export async function middleware(request: Request) {
+  // // Store current request url in a custom header, which you can read later
+  // const requestHeaders = new Headers(request.headers);
+  // requestHeaders.set("x-url", request.url);
+  let isAuth = false;
+  const cookiesStore = await cookies();
+  const authCookie = cookiesStore.get(`${process.env.AUTH_TOKEN_NAME}`);
+  const accessToken = authCookie?.value;
 
-  return NextResponse.next({
-    request: {
-      // Apply new request headers
-      headers: requestHeaders,
-    },
-  });
+  if (accessToken) isAuth = true;
+
+  console.log({ accessToken, isAuth, url: request.url });
+
+  if (isAuth) {
+    if (request.url.includes("/register")) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+
+    return NextResponse.next();
+  }
+
+  if (request.url.includes("/home"))
+    return NextResponse.redirect(new URL("/", request.url));
+
+  return NextResponse.next();
+  // return NextResponse.next({
+  //   request: {
+  //     // Apply new request headers
+  //     headers: requestHeaders,
+  //   },
+  // });
 }
