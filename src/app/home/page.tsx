20e4f57683dcc3d413fe5a5e86a5523eca1e6e8f
Helamã Ewerton <helamaewerton@gmail.com>
2024-12-29 02:58:01 -0700

    feat: Improve QR code fetching with error handling and type safety

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,20 +1,24 @@
 import { getQrCodeViewMode, getUserQrCodes } from "~/services/qrcodes/qrcodes";
+import { QrCode } from "~/services/qrcodes/qrcodes.type";
 import { NoQrCode } from "./components/no-qr-code";
 import { QRCodeDisplay } from "./components/qr-code-display";
 import { QrCodeGrid } from "./components/qr-code-display/qr-code-grid";
 import { QrCodeList } from "./components/qr-code-display/qr-code-list";
 
 export default async function Home() {
-  const qrCodes = await await (
-    await getUserQrCodes({ page: 1, limit: 10 })
-  ).data;
+  const qrCodes = await getUserQrCodes({ page: 1, limit: 10 })
+    .then((res) => res.data)
+    .catch((e) => {
+      console.log({ error: e });
+      return [] as QrCode[];
+    });
 
   const isGridMode = (await getQrCodeViewMode()) === "grid";
 
   const renderQrCodeList = () => <QrCodeGrid qrCodes={qrCodes} />;
   const renderQrCodeGrid = () => <QrCodeList qrCodes={qrCodes} />;
 
-  if (qrCodes.length === 0) return <NoQrCode />;
+  if (qrCodes?.length === 0) return <NoQrCode />;
 
   return (
     <div className="w-full h-full">
